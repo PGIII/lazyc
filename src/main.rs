6cea5b4cb87bca99ab9c42336945b7885e5ad5f3
Preston Garrison III <pg3@flightone.com>
2023-02-10 17:01:21 -0500

    start on arg defining

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use clap::{Parser, Subcommand};
+
 mod commands;
 mod cmake;
 mod terminal;
@@ -6,29 +8,43 @@ mod terminal;
 extern crate exitcode;
 
 const AVAILABLE_COMMANDS: [&str;2] = ["new", "build"];
-fn main() {
-	//let bytes = include_bytes!("../resources/test.txt");
-	//print!("{}", String::from_utf8_lossy(bytes));
-	let args: Vec<String> = env::args().collect();
-	handle_args(&args);
+
+#[derive(Parser)]
+pub struct Args {
+	#[command(subcommand)]
+	command: Commands, 
 }
 
-fn handle_args(args: &Vec<String>) {
-	let arg_count = args.len();//there will always be one argument
-	if arg_count > 1 {
-		match args[1].to_lowercase().as_str() {
-			"new" => commands::new::handle_command(args),
-			"build" | "rebuild" => commands::build::handle_command(args),
-			"help" => handle_help(args),
-			"module" => commands::module::handle_command(args),
-			"run" => commands::run::handle_command(args),
-			"configure" => commands::configure::handle_command(args),
-			"full-clean" | "clean" => commands::clean::handle_command(args),
-			_ => eprintln!("Error: Unknown Command"),
-		};
-	} else {
-		eprintln!("Error: No Arguments", );
-		std::process::exit(exitcode::USAGE);
+#[derive(Subcommand)]
+enum Commands {
+	New {
+		#[arg(short, long, help = "Name Of Project To Create")]
+		name: String,
+		#[arg(short, long)]
+		path: String,
+	},
+	Build {
+		#[arg(short, long, help = "CMake Preset To Use")]
+		preset: String,
+	},
+}
+
+fn main() {
+	let args = Args::parse();
+	handle_args();
+}
+
+fn handle_args() {
+	let args: Vec<String> = env::args().collect();
+	match env::args().nth(1).expect("Too Few Args").as_str() {
+		"new" => commands::new::handle_command(&args),
+		"build" | "rebuild" => commands::build::handle_command(&args),
+		"help" => handle_help(&args),
+		"module" => commands::module::handle_command(&args),
+		"run" => commands::run::handle_command(&args),
+		"configure" => commands::configure::handle_command(&args),
+		"full-clean" | "clean" => commands::clean::handle_command(&args),
+		_ => eprintln!("Error: Unknown Command"),
 	}
 }
 
